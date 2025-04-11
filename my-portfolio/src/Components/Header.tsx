6c1176599c6036484103ffaa4ed839d736194b40
Bemast3r <dahempvp@gmail.com>
2025-04-11 18:06:14 +0200

    Contact hinzugefügt

--- my-portfolio/src/Components/Header.tsx
+++ my-portfolio/src/Components/Header.tsx
@@ -1,16 +1,23 @@
-import { Card, CardImg } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGraduationCap, faCity } from '@fortawesome/free-solid-svg-icons';
-import '/src/stylingComponents/header.css';
-
+import { Button, ButtonGroup, Card, CardImg } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGraduationCap,
+    faCity,
+    faDotCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faXing } from "@fortawesome/free-brands-svg-icons";
+import "/src/stylingComponents/header.css";
 
 export default function Header() {
     return (
         <>
-            <div className='text'>
+            <div className="text">
                 <Card>
-
-                    <CardImg variant="top" src="https://picsum.photos/id/684/600/400" alt="Card image" />
+                    <CardImg
+                        variant="top"
+                        src="https://picsum.photos/id/684/600/400"
+                        alt="Card image"
+                    />
                     <Card.Body>
                         <div className="profile-container">
                             <img
@@ -21,14 +28,32 @@ export default function Header() {
                         </div>
                         <Card.Title className="card-title">Mohammad Hammad</Card.Title>
                         <Card.Text className="inline-text">
-                            <span><FontAwesomeIcon icon={faCity} /> Berlin</span>
-                            <span><FontAwesomeIcon icon={faGraduationCap} /> Graduated from Berliner Hochschule für Technik Berlin</span>
-                        </Card.Text>
-                        <Card.Text>
+                            <span>
+                                <FontAwesomeIcon icon={faCity} /> Berlin
+                            </span>
+                            {/* <span><FontAwesomeIcon icon={faGraduationCap} /> Graduated at <a href='https://www.bht-berlin.de/en/b-mi'>Berliner Hochschule für Technik Berlin</a></span> */}
+                            <div className="status">
+                                <span>
+                                    <FontAwesomeIcon icon={faDotCircle} />
 
+                                </span>
+                                <p>Looking for job as Software Engineer</p>
+                            </div>
+                            <div className="contact">
+                                <ButtonGroup>
+                                    <Button variant="primary" href="https://www.linkedin.com/in/mohammad-hammad-5b900b352/">
+                                        <FontAwesomeIcon icon={faLinkedin} />
+                                    </Button>
+                                    <Button variant="success" href="https://www.xing.com/profile/Mohammad_Hammad068648/web_profiles">
+                                        <FontAwesomeIcon icon={faXing} />
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
                         </Card.Text>
+                        <Card.Text></Card.Text>
                     </Card.Body>
                 </Card>
-            </div></>
-    )
+            </div>
+        </>
+    );
 }
