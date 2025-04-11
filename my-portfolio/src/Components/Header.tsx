393dbbbd5f29056fdddde151defd79906925171c
Bemast3r <dahempvp@gmail.com>
2025-04-11 16:51:19 +0200

    CSS angepasst. Struktur steht

--- my-portfolio/src/Components/Header.tsx
+++ my-portfolio/src/Components/Header.tsx
@@ -1,16 +1,31 @@
 import { Card, CardImg } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap, faCity } from '@fortawesome/free-solid-svg-icons';
 import '/src/stylingComponents/header.css';
+
 
 export default function Header() {
     return (
         <>
             <div className='text'>
                 <Card>
+
                     <CardImg variant="top" src="https://picsum.photos/id/684/600/400" alt="Card image" />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <div className="profile-container">
+                            <img
+                                src="https://picsum.photos/200" /* Beispielbild */
+                                alt="Profile"
+                                className="rounded-image"
+                            />
+                        </div>
+                        <Card.Title className="card-title">Mohammad Hammad</Card.Title>
+                        <Card.Text className="inline-text">
+                            <span><FontAwesomeIcon icon={faCity} /> Berlin</span>
+                            <span><FontAwesomeIcon icon={faGraduationCap} /> Graduated from Berliner Hochschule für Technik Berlin</span>
+                        </Card.Text>
                         <Card.Text>
-                            This is an example of a card with an image at the top.
+
                         </Card.Text>
                     </Card.Body>
                 </Card>
