b8a776e7b949d297489dc2ac40bb3bba862baf85
Bemast3r <dahempvp@gmail.com>
2025-04-20 23:36:44 +0200

    Timeline muss gemacht werden

--- my-portfolio/src/Components/Timeline.tsx
+++ my-portfolio/src/Components/Timeline.tsx
@@ -4,7 +4,12 @@ export default function Timeline() {
         <div>
             <h2>Timeline</h2>
             {/* Add a Timeline  */}
-
+            <li>
+                2019 Abitur
+            </li>
+            <li>
+                10/2021 - 02/2025 Bachelor of Science in Media Informatics
+            </li>
         </div>
     )
 }
