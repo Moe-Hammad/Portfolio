13ea579dc23a599ce628785be1743001ab9f411d
Bemast3r <dahempvp@gmail.com>
2025-04-22 22:08:45 +0200

    Card for intership

--- my-portfolio/src/Components/WorkHistory.tsx
+++ my-portfolio/src/Components/WorkHistory.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Card, CardBody, CardTitle } from "react-bootstrap";
+
+import '/src/stylingComponents/workhistory.css';
 
 export default function WorkHistory() {
     return (
-        <h2>WorkHistory</h2>
+        <>
+            <h2>Work History</h2><Card>
+                <CardTitle>
+                    SKM GmbH
+                </CardTitle>
+                <CardBody>
+                    Task :
+                </CardBody>
+            </Card>
+        </>
     )
 }
