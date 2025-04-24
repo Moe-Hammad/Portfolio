abb0c862de471a4e986080bac3c52c2e5b87ac9e
Bemast3r <dahempvp@gmail.com>
2025-04-24 20:31:48 +0200

    Card for intership. Working on an Hover Effect

--- my-portfolio/src/Components/WorkHistory.tsx
+++ my-portfolio/src/Components/WorkHistory.tsx
@@ -1,16 +1,24 @@
-import { Card, CardBody, CardTitle } from "react-bootstrap";
+import { Card, CardBody, CardSubtitle, CardTitle } from "react-bootstrap";
 
 import '/src/stylingComponents/workhistory.css';
 
 export default function WorkHistory() {
     return (
         <>
-            <h2>Work History</h2><Card>
-                <CardTitle>
-                    SKM GmbH
-                </CardTitle>
+            <h2>Work History</h2>
+            <Card className="skm">
+                <div className="skm-background">SKM</div>
+                <CardTitle>Internship</CardTitle>
+                <CardSubtitle className="skm-duration">02/24 - 05/24</CardSubtitle>
                 <CardBody>
-                    Task :
+                    <div className="work-details">
+                        <p><strong>Project:</strong> Fahrtenbuch</p>
+                        <p><strong>Goal:</strong> Digitalization of a logbook for company drivers, including graphical representation of driving, rest, and working hours with timestamps, as well as designing the company website.</p>
+                        <p><strong>Tech-Stack:</strong> MERN-Stack, Chart.js, MongoDB Atlas </p>
+                        <p><strong>My Task:</strong> Developed a feature for user authentication and implemented a trip management system.</p>
+                        <p><strong>Result:</strong> Successfully implemented and deployed the feature, improving user experience and functionality.</p>
+                        <p><strong>Company Website:</strong> <a href="https://www.e-skm.de/">SKM GmbH</a></p>
+                    </div>
                 </CardBody>
             </Card>
         </>
